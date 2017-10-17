d37a4e14842fb0100d76633b2303e654e9ae7d9f
jessbenz <jessbnsn@gmail.com>
2017-10-17 22:06:00 +0200

    Added the logo
    Back button on thanks page
    Linked the Request button to the form

--- submit.php
+++ submit.php
@@ -4,12 +4,12 @@
 if(isset($_POST['url']) && $_POST['url'] == ''){
 
 	// put the email we're sending to here
-	$youremail = 'jessbnsn@gmail.com';
+	$youremail = 'wendi.coyle@gmail.com';
 
 	// prepare a "pretty" version of the message
 	// Important: if you added any form fields to the HTML, you will need to add them here also
-	$body = "This is an expression of interest email address that was just submitted via whiterobins.com:
-	E-Mail: $_POST[email]";
+	$body = "This is an expression of interest email address that was just submitted via whiterobins.ie:
+	Name: $_POST[name] | E-Mail: $_POST[email]";
 
 	// Use the submitters email if they supplied one
 	// (and it isn't trying to hack your form).
@@ -31,14 +31,29 @@ if(isset($_POST['url']) && $_POST['url'] == ''){
 <!DOCTYPE HTML>
 <html>
 <head>
+	<title>Thanks!</title>
+	<script src="https://use.typekit.net/vfv8ksc.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+	<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/butler" type="text/css"/>
 
-<title>Thanks!</title>
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/main.css">	
 </head>
 <body>
-
-<h1>Thanks</h1>
-<p>We'll get back to you as soon as possible.</p>
-
+	<div class="thanks-page">
+		<div class="container">
+			<div class="card card-narrow text-center">
+				<h1>Thanks</h1>
+				<p class="no-margin">We'll get back to you as soon as possible.</p>	
+				<a class="btn" href="index.html">
+					<img src="images/arrow-left.svg" alt="">
+					Back
+				</a>	
+			</div>
+		</div>
+	</div>
 </body>
 </html>
